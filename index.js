29e39b4648fa2b04b62b20fe995bd6fbb539af9e
roufrubelbd <roufrubelbd@gmail.com>
2025-11-10 16:26:23 +0000

    details page and ADD modaL

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
@@ -8,55 +8,60 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tjqypvp.mongodb.net/?appName=Cluster0`;
-
-
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-const database = client.db("businessHub");
-const productsCollection = database.collection("products");
-const importsCollection = database.collection("imports");
-const exportsCollection = database.collection("exports");
+    const database = client.db("businessHub");
+    const productsCollection = database.collection("products");
+    const importsCollection = database.collection("imports");
+    const exportsCollection = database.collection("exports");
 
+    app.get("/products", async (req, res) => {
+      try {
+        const products = await productsCollection.find({}).toArray();
+        res.send(products);
+      } catch (err) {
+        // console.error(err);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
 
-
-app.get('/products', async (req, res) => {
-  try {
-    const products = await productsCollection.find({}).toArray();
-    res.send(products);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: 'Server error' });
-  }
-});
-
-
+    app.get("/products/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const product = await productsCollection.findOne({
+          _id: new ObjectId(id),
+        });
+        if (!product)
+          return res.status(404).send({ message: "Product not found" });
+        res.send(product);
+      } catch (err) {
+        // console.error(err);
+        res.status(500).send({ message: "Server error" });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
